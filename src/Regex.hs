03fa3eba07b19538072086f444b2a00ed4a61c63
Florian Kahllund <flo.kahllund@gmail.com>
2016-03-27 21:53:48 +0200

    restructured parsing of the regular expression

--- src/Regex.hs
+++ src/Regex.hs
@@ -1,13 +1,11 @@
 module Regex where
 
 import Text.ParserCombinators.Parsec
-
-
+import Control.Applicative ((<**>))
 
 data Regex = List [Regex]
            | Negate Regex
            | Or [Regex]
-           | And [Regex]
            | Many Regex      -- ^ 0 or more
            | Some Regex      -- ^ 1 or more
            | ZeroOrOne Regex
@@ -16,27 +14,28 @@ data Regex = List [Regex]
            deriving Show
 
 readRegex :: String -> String
-readRegex str = case parse parseRegex "regex" str of
+readRegex str = case parse parseFullRegex "regex" str of
                 Left err  -> "error: " ++ show err
                 Right val -> show val
 
+--TODO remove after testing
+test :: String -> IO ()
+test = putStrLn . readRegex
+
+parseFullRegex :: Parser Regex
+parseFullRegex = parseRegex >>= \x -> eof >> return x
+
 parseRegex :: Parser Regex
-parseRegex = f <$> many parseMultiRegex
+parseRegex = f <$> many1 (choice regexParsers)
   where
     f :: [Regex] -> Regex
     f [x] = x
     f xs  = List xs
 
-parseMultiRegex :: Parser Regex
-parseMultiRegex = parseOr
-              <|> try parseMany
-              <|> try parseSome
-              <|> try parseZeroOrOne
-              <|> parseAnyCharacter
-              <|> parseCharacter
-
-parseSingleRegex :: Parser Regex
-parseSingleRegex = parseAnyCharacter <|> parseCharacter
+regexParsers :: [Parser Regex]
+regexParsers = map (<**> parseFollowChar) [parseOr,
+                                           parseDot,
+                                           parseCharacter]
 
 parseList :: Parser Regex
 parseList = List <$> many parseRegex
@@ -44,25 +43,14 @@ parseList = List <$> many parseRegex
 parseOr :: Parser Regex
 parseOr = Or <$> between (char '(') (char ')') (sepBy1 parseRegex (char '|'))
 
-followedByChar :: (Regex -> r) -> Char -> Parser r
-followedByChar f c = do a <- parseSingleRegex
-                        _ <- char c
-                        return $ f a
-
-parseMany :: Parser Regex
-parseMany = followedByChar Many '*'
-
-parseSome :: Parser Regex
-parseSome = followedByChar Some '+'
-
-parseZeroOrOne :: Parser Regex
-parseZeroOrOne = followedByChar ZeroOrOne '?'
-
-operators :: String
-operators = "|()*+"
+parseFollowChar :: Parser (Regex -> Regex)
+parseFollowChar = (char '*' >> return Many)
+              <|> (char '+' >> return Some)
+              <|> (char '?' >> return ZeroOrOne)
+              <|> return id
 
 parseCharacter :: Parser Regex
-parseCharacter = Character <$> noneOf operators
+parseCharacter = Character <$> noneOf "^[.${*(\\+)|?<>"
 
-parseAnyCharacter :: Parser Regex
-parseAnyCharacter = char '.' >> return AnyCharacter
+parseDot :: Parser Regex
+parseDot = char '.' >> return AnyCharacter
