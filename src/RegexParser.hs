d019bbcff63d99622da77c9067882badc01be169
Florian Kahllund <flo.kahllund@gmail.com>
2016-04-05 18:31:16 +0300

    renaming some functions and documenting RegexParser

--- src/RegexParser.hs
+++ src/RegexParser.hs
@@ -1,9 +1,15 @@
-module RegexParser where
+-- | RegexParser contains Parsers for Regexes.
+module RegexParser ( regexes2Parser,
+                     checkRegexes,
+                     checkString,
+                     checkFullString
+                   ) where
 
 import Regex
 import Control.Monad ( void )
 import Text.ParserCombinators.Parsec
 
+-- | regexes2Parser rs builds a Parser from rs.
 regexes2Parser :: [Regex] -> Parser ()
 regexes2Parser []                     = return ()
 regexes2Parser (List rs : rss)        = regexes2Parser rs >> regexes2Parser rss
@@ -22,20 +28,25 @@ regexes2Parser (MinMax i j r : rs)
 regexes2Parser (Character r : rs)     = void (char r) >> regexes2Parser rs
 regexes2Parser (AnyCharacter : rs)    = void anyChar >> regexes2Parser rs
 
+-- | regex2Parser r builds a Parser for the single Regex r.
 regex2Parser :: Regex -> Parser ()
 regex2Parser = regexes2Parser . return
 
+-- | parseFullString p fist applies p and then eof.
 parseFullString :: Parser a -> Parser a
 parseFullString p = do { x <- p; _ <- eof; return x }
 
-test :: [Regex] -> String -> Either ParseError ()
-test rs = parse (regexes2Parser rs) "test" 
+-- | checkRegexes rs str builds the Parser for rs and then parses str.
+checkRegexes :: [Regex] -> String -> Either ParseError ()
+checkRegexes rs = parse (regexes2Parser rs) "apply regex" 
 
-testRegex :: String -> String -> Either ParseError ()
-testRegex rs cs = do regexes <- readRegex rs
-                     parse (regexes2Parser regexes) "apply" cs
+-- | checkString rs str builds a Parser from rs and then parsers str.
+checkString :: String -> String -> Either ParseError ()
+checkString rs cs = do regexes <- readRegex rs
+                       parse (regexes2Parser regexes) "apply regex" cs
 
-ca :: Regex
-ca = Character 'a'
-cb :: Regex
-cb = Character 'b'
+-- | As checkString, but fails if the regex doesn't apply to the hole string.
+checkFullString :: String -> String -> Either ParseError ()
+checkFullString rs cs = do regexes <- readRegex rs
+                           parse (parseFullString (regexes2Parser regexes))
+                                 "apply regex" cs
